f9c481970e0390186fec7edf82ce0c9d177f05db
R.A Borja <raborja.dev@gmail.com>
2022-09-13 21:23:22 +0800

    create array and function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// array for books
+const myLibrary = [];
+
 function Book(title, author, pages, isRead = false) {
   this.title = title;
   this.author = author;
@@ -7,4 +10,16 @@ function Book(title, author, pages, isRead = false) {
   this.info = function() {
     return `${this.title}, by ${this.author}, ${pages} pages, ${(this.isRead) ? 'have read' : 'not read yet'}`;
   }
+}
+
+
+function addBookToLibrary() {
+  // take user input
+  let title = prompt("Enter book title:");
+  let author = prompt("Enter author:");
+  let pages = prompt("Enter number of pages:");
+  let isRead = confirm("Have you read read the book?");
+
+  // add book to array
+  myLibrary.push(new Book(title, author, pages, isRead));
 }
