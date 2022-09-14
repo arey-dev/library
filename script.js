3426410684deaa8dc069432a8d681510cdbb80a0
R.A Borja <raborja.dev@gmail.com>
2022-09-14 14:44:31 +0800

    add function: generateCard(), showBooks()

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // array for books
 const myLibrary = [];
+const book1 = new Book("To Kill A Mockingbird", "Harper Lee", "281", false);
+const book2 = new Book("Gone Girl", "Gillian Flynn", "432", true);
+myLibrary.push(book1);
+myLibrary.push(book2);
 
 function Book(title, author, pages, isRead = false) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead; 
-
-  this.info = function() {
-    return `${this.title}, by ${this.author}, ${pages} pages, ${(this.isRead) ? 'have read' : 'not read yet'}`;
-  }
 }
 
 function addBookToLibrary() {
@@ -23,6 +23,42 @@ function addBookToLibrary() {
   myLibrary.push(new Book(title, author, pages, isRead));
 }
 
+// generate a card for book
+// accepts a Book obj
+// returns a an article element with book info
+function generateCard(book) {
+  const card = document.createElement('article');
+  card.classList.add('book');
+  card.classList.add('flex');
+
+  const h3_title = document.createElement('h3');
+  h3_title.innerHTML = `<em class="book-info">${book.title}</em>`;
+  card.append(h3_title);
+
+  const p_author = document.createElement('p');
+  p_author.innerHTML = `Author: <em class="book-info">${book.author}</em>`;
+  card.append(p_author);
+
+  const p_pages = document.createElement('p');
+  p_pages.innerHTML = `Pages: <em class="book-info">${book.pages}</em>`;
+  card.append(p_pages);
+
+  const p_status = document.createElement('p');
+  const status = (book.isRead) ? 'Finished' : 'Not Finished';
+  p_status.innerHTML = `Status: <em class="book-info">${status}</em>`;
+  card.append(p_status);
+
+  return card;
+}
+
 function showBooks() {
-  myLibrary.forEach(book => console.log(book));
+  const shelve = document.querySelector('.grid-wrapper');
+  shelve.innerHTML = ''; // resets the container
+
+  const books_length = myLibrary.length;
+  for(let i = 0; i < books_length; i++) {
+    const card = generateCard(myLibrary[i]); 
+
+    shelve.append(card);
+  }
 }
